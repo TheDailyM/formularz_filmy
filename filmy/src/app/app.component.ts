1339719bbc2b8b11e1442473246b5be67d2ee6b5
TheDailyM <tomekkaniuk99@gmail.com>
2025-05-14 14:50:03 +0200

    dokończone ig

--- filmy/src/app/app.component.ts
+++ filmy/src/app/app.component.ts
@@ -1,20 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'filmy';
-}
 
-let nazwaFilmu = "";
-let kategoriaFilmu = "";
+   nazwaFilmu = "";
+   kategoriaFilmu = "";
 
-function dodajFilm(arg1:string, arg2:string){
+  dodajFilm(arg1:string, arg2:string){
+      console.log("{tytul: "+ this.nazwaFilmu +", kategoria: "+ this.kategoriaFilmu+"}");
+  }
 
+  changeName($event:any){
+      this.nazwaFilmu = $event.target.value;
+  }
+
+  changeType($event:any){
+      this.kategoriaFilmu = $event.target.value;
+  }
 }
